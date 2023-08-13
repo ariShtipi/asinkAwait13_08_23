8e877cfcb555e710a4b58842d3426ea45a58af57
ari shtipi <arish186777@gmail.com>
2023-08-13 15:31:47 +0300

    hello world

---
+++ asyncClient.js
@@ -0,0 +1,54 @@
+const buttonGetRandomUser = document.createElement("button");
+buttonGetRandomUser.innerText = "Get random user";
+document.body.appendChild(buttonGetRandomUser);
+
+buttonGetRandomUser.addEventListener("click", () => {
+  getRandomUser();
+});
+
+const buttonGet5Users = document.createElement("button");
+buttonGet5Users.innerText = "Get random user";
+document.body.appendChild(buttonGet5Users);
+
+buttonGet5Users.addEventListener("click", () => {
+  get5User();
+});
+
+async function getRandomUser() {
+  try {
+    const response = await fetch("https://randomuser.me/api");
+    if (response.ok) {
+      const data = await response.json();
+      console.log(
+        data.results[0].name.first +
+          "  " +
+          data.results[0].name.last +
+          "  " +
+          data.results[0].email +
+          "  " +
+          data.results[0].registered.age +
+          "  " +
+          data.results[0].picture.thumbnail
+      );
+    } else {
+      throw new Error("error");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function get5User() {
+  try {
+    const response = await fetch(
+      "https://randomuser.me/api/?gender=male&results=5"
+    );
+    if (!response.ok) {
+      throw new Error("error");
+    }
+    const data = await response.json();
+    console.log(data.results);
+  } catch (error) {
+    console.error("error:", error.message);
+  }
+}
